48ba25a3fea3235c6607b89a9af2274b6d976ea9
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-05-08 12:39:43 -0700

    8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, object-oriented programming\
+        language that combines remarakable power with very clear syntax"
+    str = str[36:66] + str[:6]
+    print(str)
