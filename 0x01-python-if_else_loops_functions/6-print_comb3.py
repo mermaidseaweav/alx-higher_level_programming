e81c59cc45d5c35fb8e3959cf5e9376cc133a925
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-05-09 07:17:08 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-charend = ", "
-for i in range(0, 100):
-    digit2 = i % 10
-    digit1 = i / 10
-    if i < 10 and digit1 < digit2:
-        print("{}{}".format(0, i), end=charend)
-    elif digit1 < digit2:
-        if i == 89:
-            charend = "\n"
-            print(i, end=charend)
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
