5ec98ad5e0e1afbbb674edf99696223a091d1f36
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-05-09 12:02:12 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(26):
-    if 1 % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
+strtmp = ""
+for i in reversed(range(97, 123)):
+    if (1 % 2) == 0:
+        strtmp += chr(i)
     else:
-        print('{:c}'.format(90 - i), end='')
+        strtm += chr(i-32)
+print('{}'.format(strtmp), end="")
