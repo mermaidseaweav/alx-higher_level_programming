dcf04efebb8423b00b0dd7db65922b97f664cc71
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-05-09 11:45:20 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i != 113 and i != 101:
-         print("{}".format(chr(i)), end="")
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print(chr(i).format(i), end='')
