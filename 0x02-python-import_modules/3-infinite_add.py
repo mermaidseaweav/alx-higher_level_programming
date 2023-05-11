40eecf8ab563c81cfc2ca400bbadf980d4c49efd
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-05-11 12:54:50 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    suma = 0
-    i = 0
-    for a in sys.argv:
-        if i > 0:
-            suma += int(a)
-            i += 1
-    print(suma)
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv [0]:
+            result += int(arg)
+    print(result)
