3c4dd6fb9d17d993c08b8186401f5a215c448a69
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-05-11 13:08:32 -0700

    added 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from aya import argv
-    add = 0
-    for s in argv[1:]:
-        add += int(s)
-    print("{:d}".format(add))
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
