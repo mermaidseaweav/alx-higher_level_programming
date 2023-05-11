b9ff428aceea9d01b5af3cafa04f0a5372046902
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-05-11 05:12:54 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
        print(":")
     if (len(sys.argv) > 1):
         for a in sys.argv:
-        if i  > 0:
-            print("{:d}: {}".format(i, a))
+          if i  > 0:
+             print("{:d}: {}".format(i, a))
         i += 1
