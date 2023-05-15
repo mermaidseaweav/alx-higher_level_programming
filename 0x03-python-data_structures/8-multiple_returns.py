759aca2d28eb07fa722bfb4fcde499f38e433f28
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-05-15 12:29:47 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,7 @@ def nultiple_returns(sentence):
 
     if (len_sen == 0):
         new_tuple = (len_sen, None)
+        return new_tuple
     else:
-        new_tuple = (len_sen, sentence[0])
+        new_tuple = (len_sen, sentence[0:1])
     return (new_tuple)
